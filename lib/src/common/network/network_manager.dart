edcdcd469001e987f5bbc784daea8d20c41684a6
Edouard Marquez <moi@edouard-marquez.me>
2020-08-18 11:25:01 +0200

    Device & app info

--- lib/src/common/network/network_manager.dart
+++ lib/src/common/network/network_manager.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:device_info/device_info.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:http_parser/http_parser.dart';
+import 'package:package_info/package_info.dart';
 import 'package:wiredash/src/common/network/api_client.dart';
 
 class NetworkManager {
@@ -17,6 +19,17 @@ class NetworkManager {
   static const String _parameterEmail = 'email';
   static const String _parameterPayload = 'payload';
   static const String _parameterUser = 'user';
+
+  static const String _parameterPackageAppName = 'package_app_name';
+  static const String _parameterPackageVersion = 'package_version';
+  static const String _parameterPackageBuildNumber = 'package_build_number';
+  static const String _parameterPackageName = 'package_name';
+
+  static const String _parameterDeviceProduct = 'device_product';
+  static const String _parameterDeviceModel = 'device_model';
+  static const String _parameterDeviceBrand = 'device_brand';
+  static const String _parameterDevicePhysicalDevice = 'device_physical_device';
+  static const String _parameterDeviceVersion = 'device_version';
 
   static const String _parameterFeedbackMessage = 'message';
   static const String _parameterFeedbackScreenshot = 'file';
@@ -32,6 +45,10 @@ class NetworkManager {
     String user,
   }) async {
     MultipartFile screenshotFile;
+
+    final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    AndroidDeviceInfo androidInfo = await DeviceInfoPlugin().androidInfo;
+    IosDeviceInfo iosInfo = await DeviceInfoPlugin().iosInfo;
 
     if (picture != null) {
       screenshotFile = MultipartFile.fromBytes(
@@ -50,7 +67,19 @@ class NetworkManager {
         _parameterFeedbackMessage: message,
         if (payload != null) _parameterPayload: json.encode(payload),
         _parameterFeedbackType: type,
-        if (user != null) _parameterUser: user
+        if (user != null) _parameterUser: user,
+        _parameterPackageAppName: packageInfo?.appName,
+        _parameterPackageVersion: packageInfo?.version,
+        _parameterPackageBuildNumber: packageInfo?.buildNumber,
+        _parameterPackageName: packageInfo?.packageName,
+        _parameterDeviceProduct: androidInfo?.product ?? iosInfo?.model,
+        _parameterDeviceModel: androidInfo?.model ?? iosInfo?.localizedModel,
+        _parameterDeviceBrand: androidInfo?.brand ?? 'Apple',
+        _parameterDevicePhysicalDevice:
+            (androidInfo?.isPhysicalDevice ?? iosInfo?.isPhysicalDevice)
+                ?.toString(),
+        _parameterDeviceVersion:
+            androidInfo?.version?.sdkInt?.toString() ?? iosInfo?.systemVersion,
       },
       files: [screenshotFile],
     );
